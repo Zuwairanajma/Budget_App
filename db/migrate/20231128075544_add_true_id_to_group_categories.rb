618edcfb8bbd4c2a5ff3376353f9b5e5c4909fae
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-11-29 19:07:18 +0100

    Remove true id obstruction from migration file

--- db/migrate/20231128075544_add_true_id_to_group_categories.rb
+++ db/migrate/20231128075544_add_true_id_to_group_categories.rb
@@ -1,5 +1,5 @@
-class AddTrueIdToGroupCategories < ActiveRecord::Migration[7.1]
-  def change
-    add_column :group_categories, :true_id, :integer
-  end
-end
+# class AddTrueIdToGroupCategories < ActiveRecord::Migration[7.1]
+#   def change
+#     add_column :group_categories, :true_id, :integer
+#   end
+# end
